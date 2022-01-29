331ebc5f2266ac3c6263e212344e800e2a9c309a
stldo <st.shared@icloud.com>
2022-01-29 00:50:02 -0300

    refactor: disable symbolPropertyName option

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,46 +2,38 @@ const SvgStorePlugin = require('external-svg-sprite-loader')
 const { resolve } = require('path')
 
 const GatsbyTypescriptPluginFix = require('./lib/gatsby-typescript-plugin-fix')
-const getOptimizedIconName = require('./lib/get-optimized-icon-name')
-
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const getMinifiedId = require('./lib/get-minified-id')
 
 exports.onCreateWebpackConfig = ({ actions, getConfig, rules }, {
-  optimize = IS_PRODUCTION,
+  iconName = '[name]--[hash:base64:5]',
+  minifyIds = process.env.NODE_ENV === 'production',
   pluginOptions = {},
   plugins: _,
-  symbolPropertyName,
   ...externalSvgSpriteLoaderOptions
 }) => {
   const config = getConfig()
   const imagesRule = rules.images()
   const imagesRuleTest = String(imagesRule.test)
 
-  const rule = {
-    test: /\.svg$/,
-    use: [{
-      loader: SvgStorePlugin.loader,
-      options: {
-        name: 'sprites.[contenthash].svg',
-        iconName: optimize ? getOptimizedIconName : '[name]--[hash:base64:5]',
-        ...externalSvgSpriteLoaderOptions
-      }
-    }]
-  }
-
-  if (symbolPropertyName) {
-    rule.use.unshift({
-      loader: resolve(__dirname, 'lib', 'symbol-property-name.js'),
-      options: { symbolPropertyName }
-    })
-  }
-
   config.module.rules = [
     ...config.module.rules.filter(rule => (
       String(rule.test) !== imagesRuleTest
     )),
 
-    rule,
+    {
+      test: /\.svg$/,
+      use: [{
+        loader: resolve(__dirname, 'lib', 'symbol-property-name.js'),
+        options: { symbolPropertyName: 'url' }
+      }, {
+        loader: SvgStorePlugin.loader,
+        options: {
+          name: 'sprites.[contenthash].svg',
+          iconName: minifyIds ? getMinifiedId : iconName,
+          ...externalSvgSpriteLoaderOptions
+        }
+      }]
+    },
 
     {
       ...imagesRule,
